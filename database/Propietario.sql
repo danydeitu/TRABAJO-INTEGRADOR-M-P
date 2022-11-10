0177753d527bec3c89645487d38d12980e80c788
Daniel <danydeitu18@gmail.com>
2022-11-10 08:52:36 -0300

    DB

---
+++ database/Propietario.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE proyectoIntegradorv01;
+
+USE proyectoIntegradorv01;
+
+CREATE TABLE `Propietario`(
+`id_Propietario` INT,
+`Nombre` varchar(50),
+`Apellido` varchar(50),
+`FechaNacimiento` datetime,
+PRIMARY KEY (`id_Propietario`)
+)
